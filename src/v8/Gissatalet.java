d5311e78aa4ef0c6b06392a4458d71808a65232e
Mattias <Mattias.andersson@elev.ga.ntig.se>
2019-02-25 10:57:19 +0100

    Update Gissatalet.java

--- src/v8/Gissatalet.java
+++ src/v8/Gissatalet.java
@@ -6,60 +6,72 @@ import java.util.Random;
 public class Gissatalet {
 
 	public static void main(String[] args) {
-
-		Random Random = new Random();
-		
-		
-		int numberToGuess = Random.nextInt(50 + 1);
-		int numberOfTries = 0;
 		Scanner Scanner = new Scanner(System.in);
+		
+		int difficulty = 0;
+		System.out.println("Type your desired guessing range( between 0 and X)");
+		difficulty = Scanner.nextInt();
+		
+		
+		Random Random = new Random();
+
+		int numberToGuess = Random.nextInt(difficulty + 1);
+		int numberOfTries = 0;
+		int numberOfTriesTotal = 0;
+		int games = 0;
 		int guess;
 		boolean win = false;
 		boolean running = true;
-		
-		while(running) {
-		
-		System.out.println("Guess a number between 1 and 50");
 
-		while(win == false){
-			guess = Scanner.nextInt();
-			numberOfTries ++;
-			
-			if(guess == numberToGuess) {
-				win = true;	
+		while (running) {
+
+			System.out.println("Guess a number between 0 and " + difficulty);
+
+			while (win == false) {
+				guess = Scanner.nextInt();
+				numberOfTries++;
+				numberOfTriesTotal++;
+
+				if (guess == numberToGuess) {
+					win = true;
+					games ++;
+				} else if (guess < numberToGuess) {
+					System.out.println("Your guess is too low");
+				} else if (guess > numberToGuess) {
+					System.out.println("Your guess is too high");
+				}
+
 			}
-			else if(guess < numberToGuess) {
-				System.out.println("Your guess is too low");
-			}
-			else if(guess > numberToGuess) {
-				System.out.println("Your guess is too high");
-			}
+			System.out.println("You Have Won!");
+			System.out.println("You guessed " + numberOfTries + " Times");
+			System.out.println("You Have guessed " + numberOfTriesTotal + " Times In Total");
 			
+			System.out.println("");
 			
-		}
-		System.out.println("You Have Won!");
-		System.out.println("You guessed " + numberOfTries + " Times");
-		
-		boolean keepplaying = false;
-		
-		while(keepplaying == false) {
 			System.out.println("Press 1 if you would like to play again");
 			System.out.println("Type 2 if you would like to quit");
-			
+
 			int choice = Scanner.nextInt();
 			switch (choice) {
-			case 1: 
-				keepplaying = true;
-				
-			case 2: 
+			case 1:
+				win = false;
+				numberToGuess = Random.nextInt(difficulty + 1);
+				numberOfTries = 0;
+				difficulty = 0;
+				System.out.println("Type your desired guessing range(0 and X)");
+				difficulty = Scanner.nextInt();
+				break;
+			case 2:
 				System.out.println("Thanks for playing");
-				keepplaying = false;
-				running = false;
-			}
-		}
-			
-		}
-		
-	}
+				System.out.println("Here are your final results:");
+				System.out.println("You guessed " + numberOfTriesTotal + " times and played " + games);
 
+				running = false;
+				break;
+
+			}
+
+		}
+
+	}
 }
